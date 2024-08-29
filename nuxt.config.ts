ac3a90ce00ac0ad00d888b1d98d198bba17ad1ab
Marcelo Tosco <marcelo.tosco@1xinternet.de>
2024-08-29 02:38:53 +0200

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    compatibilityDate: '2024-08-29',
     devtools: {enabled: false},
     modules: [
         '@nuxtjs/sanity',
